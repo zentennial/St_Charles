7f905553323af510919ad036c81a17e6783cf1fd
zentennial <zwade17@gmail.com>
2013-08-20 20:41:05 -0500

    white space

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function loadRepos() {
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
         var i, repo;
         $.each(data.moduleArr, function (i, story) {
-            $("#allRepos").append("<li><a href='https://github.com/" + story.headline + "/" + story.headline + "'>"
+            $("#allRepos").append("<li style = 'white-space:normal'><a href='https://github.com/" + story.headline + "/" + story.headline + "'>"
             + "<img src = '" + story.thumb + "'></img>"
 		    + "<h4>" + story.headline + "</h4>"
             + "<p>" + story.headline + "</p></a></li>");
