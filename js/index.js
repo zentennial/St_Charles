766ff9f7a7443070dbe99ab3963d004b9d464bc5
zentennial <zwade17@gmail.com>
2013-08-20 20:47:33 -0500

    thumb and text wrap

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ function loadRepos() {
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
         var i, repo;
         $.each(data.moduleArr, function (i, story) {
-            $("#allRepos").append("<li><a style = 'white-space:normal' href='https://github.com/" + story.headline + "/" + story.headline + "'>"
-            + "<img src = '" + story.thumb + "'></img>"
+            $("#allRepos").append("<li><div><a' href='https://github.com/" + story.headline + "/" + story.headline + "'></div>"
+            + "<img src = '" + story.thumb + "' class = 'thumb'></img>"
 		    + "<h4>" + story.headline + "</h4>"
             + "<p>" + story.headline + "</p></a></li>");
         });
