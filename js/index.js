106c786fe7b4e5dfa43feceb1317aeef91582c35
zentennial <zwade17@gmail.com>
2013-08-20 22:11:09 -0500

    min height

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function loadRepos() {
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
         var i, repo;
         $.each(data.moduleArr, function (i, story) {
-            $("#allRepos").append("<li><div style = 'white-space:normal;'><a' href='https://github.com/" + story.headline + "/" + story.headline + "'></div>"
+            $("#allRepos").append("<li style = 'min-height:90px;'><a' href='https://github.com/" + story.headline + "/" + story.headline + "'>"
             + "<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
 		    + "<h4>" + story.headline + "</h4>"
             + "<p>" + story.headline + "</p></a></li>");
