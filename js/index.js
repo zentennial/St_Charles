c51bf244849f10623e23efee4f8e513b633c95fb
zentennial <zwade17@gmail.com>
2013-08-22 18:30:52 -0500

    Try

--- js/index.js
+++ js/index.js
@@ -6,9 +6,9 @@ function loadRepos() {
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
         var i, repo;
         $.each(data.moduleArr, function (i, story) {
-            $("#allRepos").append("<li style = 'min-height:90px;'><a' href='https://github.com/" + story.headline + "/" + story.headline + "'>"
+            $("#allRepos").append("<li style = 'min-height:80px;'><a' href='https://github.com/'>"
             + "<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
-		    + "<h4>" + story.headline + "</h4>"
+		    + "<div>" + story.headline + "</div>"
             + "<p>" + story.headline + "</p></a></li>");
         });
         $('#allRepos').listview('refresh');
