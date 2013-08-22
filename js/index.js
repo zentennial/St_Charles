15acdf621368b893522cf14677dc72be2ec5ca71
zentennial <zwade17@gmail.com>
2013-08-22 18:52:47 -0500

    no faulty stories

--- js/index.js
+++ js/index.js
@@ -6,10 +6,13 @@ function loadRepos() {
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
         var i, repo;
         $.each(data.moduleArr, function (i, story) {
-            $("#allRepos").append("<li style = 'min-height:80px;'><a' href='https://github.com/'>"
-            + "<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
-		    + "<div class = 'listHeadline'>" + story.headline + "</div>"
-            + "<p>" + story.headline + "</p></a></li>");
+										 
+		  if(story.headline){						 
+               $("#allRepos").append("<li style = 'min-height:80px;'><a href='https://github.com/'>"
+               + "<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
+		       + "<div class = 'listHeadline'>" + story.headline + "</div>"
+                + "<p>" + story.headline + "</p></a></li>");
+		  }
         });
         $('#allRepos').listview('refresh');
     });
