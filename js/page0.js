b8f6fc9d79512928c5a575dbaa5190ef3b90d315
zentennial <zwade17@gmail.com>
2013-12-30 13:58:10 -0600

    fdsf

--- js/page0.js
+++ js/page0.js
@@ -1,13 +1,13 @@
 $('#reposHome').bind('pageinit', function(event) {
-	loadRepos();
+	loadRepos0();
 });
 
-function loadRepos() {
+function loadRepos0() {
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
         var i, repo;
         $.each(data.moduleArr, function (i, story) {
 										 
-		  if(i == 0){						 
+		  if(i){						 
                $("#allRepos").append("<li style = 'min-height:80px;'>"
                +"<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
 			   +"<div id = 'listText'>"
