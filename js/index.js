46fd1b1470c94956cbcca204fbdbfc82159cb06b
zentennial <zwade17@gmail.com>
2013-12-30 15:47:50 -0600

    dsf

--- js/index.js
+++ js/index.js
@@ -28,10 +28,10 @@ $('#reposArticle').bind('pageinit', function(event) {
 
 function loadRepos0(){
     $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
-        var i, repo;
-        $.each(data.moduleArr, function (i, story) {
+        var j, repo;
+        $.each(data.moduleArr, function (j, story) {
 										 
-		  if(i){						 
+		  if(j){						 
                $("#storyRepos").append("<li style = 'min-height:80px;'>"
                +"<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
 			   +"<div id = 'listText'>"
