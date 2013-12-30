fcc85195514d8e933441a1ba00744726ff6c1bae
zentennial <zwade17@gmail.com>
2013-12-30 15:28:38 -0600

    dsfg

--- js/index.js
+++ js/index.js
@@ -19,3 +19,27 @@ function loadRepos() {
     }); 
 }
 
+
+
+
+
+$('#reposArticle').bind('pageinit', function(event) {
+	loadRepos0();
+});
+
+function loadRepos0(){
+    $.ajax("http://jhmc-r7zk.accessdomain.com/lindsay/stcharles_build.json").done(function(data) {
+        var i, repo;
+        $.each(data.moduleArr, function (i, story) {
+										 
+		  if(i){						 
+               $("#storyRepos").append("<li style = 'min-height:80px;'>"
+               +"<div style = 'background-image:url(" + story.thumb + ");' class = 'thumb'></div>"
+			   +"<div id = 'listText'>"
+		       +"<div class = 'listHeadline'>" + story.headline + "</div>"
+               +"<div class = 'listSummary'>" + story.summary + "</div></div></li>");
+		  }
+        });
+        $('#storyRepos').listview('refresh');
+    }); 
+}
